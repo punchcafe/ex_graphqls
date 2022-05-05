c1f2d5533de16b4480ff654ab2051a5c7ea9502f
Luca Fruzza <lfruzza@hotmail.co.uk>
2022-05-05 23:55:59 +0100

    :sparkles: add token whitespace splitter utility

---
+++ test/utility_test.exs
@@ -0,0 +1,17 @@
+defmodule ExGraphqls.UtilityTest do
+    use ExUnit.Case, async: false
+
+    alias Graphqls.Utility
+
+    describe  "split_by_spaces/2" do
+
+        test "splits by whitespace" do
+            sample = """
+            The quick brown fox jumped over something I can't remember.
+            """ |> String.to_charlist()
+
+            assert Utility.split_by_spaces(sample, 2) == {['The', 'quick'], 'brown fox jumped over something I can\'t remember.\n'}
+        end
+        
+    end
+end
